7dc38393ef04e13eef7e6c8f07cafbea298c9309
DeGobbi <andre10degobi@gmail.com>
2025-09-08 22:43:27 -0300

    feat: inicializa projeto com backend Spring Boot e frontend Angular

---
+++ backend/src/main/java/com/prysma/prysma/PrysmaApplication.java
@@ -0,0 +1,13 @@
+package com.prysma.prysma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrysmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrysmaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/prysma/prysma/PrysmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prysma.prysma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrysmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
